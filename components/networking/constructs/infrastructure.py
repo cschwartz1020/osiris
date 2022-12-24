23a365a5a21972de0ff686096a6c7e5bdd5bf860
Charlie Schwartz <cschwartz1020@gmail.com>
2022-12-24 15:45:41 -0700

    tweak vpc setup, add destroy workflow, comment out multi-region

--- components/networking/constructs/infrastructure.py
+++ components/networking/constructs/infrastructure.py
@@ -15,32 +15,21 @@ class VPC(Construct):
         self.vpc = ec2.Vpc(self, env_name,
                            ip_addresses=ec2.IpAddresses.cidr(
                                '192.168.50.0/24'),
-                           max_azs=2,
+                           max_azs=3,
                            enable_dns_hostnames=True,
                            enable_dns_support=True,
                            subnet_configuration=[
                                ec2.SubnetConfiguration(
-                                   name='Public-Subnet',
-                                   subnet_type=ec2.SubnetType.PUBLIC,
-                                   cidr_mask=26
+                                   name='dmz',
+                                   subnet_type=ec2.SubnetType.PUBLIC
                                ),
                                ec2.SubnetConfiguration(
-                                   name='Private-Subnet',
-                                   subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
-                                   cidr_mask=26
+                                   name='app',
+                                   subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+                               ),
+                               ec2.SubnetConfiguration(
+                                   name='db',
+                                   subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
                                )
                            ],
-                           nat_gateways=1,
                            )
-
-        priv_subnets = [
-            subnet.subnet_id for subnet in self.vpc.private_subnets]
-
-        count = 1
-        for psub in priv_subnets:
-            ssm.StringParameter(self, 'private-subnet-' + str(count),
-                                string_value=psub,
-                                parameter_name='/'+env_name +
-                                '/private-subnet-'+str(count)
-                                )
-            count += 1
