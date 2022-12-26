12b0da7c4c8e8ed12182bcd68e9f4a86e330ff15
Charlie Schwartz <cschwartz1020@gmail.com>
2022-12-25 19:58:51 -0700

    add lt

--- infrastructure/constructs/compute/eks.py
+++ infrastructure/constructs/compute/eks.py
@@ -1,42 +1,66 @@
-from aws_cdk import (
-    aws_eks as eks,
-    aws_ec2 as ec2,
-    aws_iam as iam
-)
+from aws_cdk import aws_eks as eks, aws_ec2 as ec2, aws_iam as iam, CfnTag as tag
 from constructs import Construct
 from ..networking.vpc import VPC
 
 
 class EKS(Construct):
-    def __init__(self, scope: Construct, id: str, *, Vpc: VPC) -> None:
+    def __init__(
+        self, scope: Construct, id: str, *, Vpc: VPC, app_subnets: ec2.SubnetSelection
+    ) -> None:
         super().__init__(scope, id)
 
         env_name = self.node.try_get_context("env")
 
         # Using spot instances and T2 micros to save on AWS costs
         # m5.large is probably suitable for real production workloads
-        self.cluster = eks.Cluster(self, env_name,
-                                   cluster_name=env_name,
-                                   version=eks.KubernetesVersion.V1_23,
-                                   vpc=Vpc.vpc,
-                                   default_capacity=0,
-                                   default_capacity_type=eks.DefaultCapacityType.NODEGROUP,
-                                   default_capacity_instance=ec2.InstanceType.of(
-                                       ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-                                   alb_controller=eks.AlbControllerOptions(
-                                       version=eks.AlbControllerVersion.V2_4_1
-                                   )
-                                   )
+        self.cluster = eks.Cluster(
+            self,
+            env_name,
+            cluster_name=env_name,
+            version=eks.KubernetesVersion.V1_23,
+            vpc=Vpc.vpc,
+            default_capacity=0,
+            default_capacity_type=eks.DefaultCapacityType.NODEGROUP,
+            default_capacity_instance=ec2.InstanceType.of(
+                ec2.InstanceClass.T2, ec2.InstanceSize.MICRO
+            ),
+            alb_controller=eks.AlbControllerOptions(
+                version=eks.AlbControllerVersion.V2_4_1
+            ),
+        )
 
-        self.cluster.add_nodegroup_capacity("spot",
-                                            instance_types=[
-                                                ec2.InstanceType.of(ec2.InstanceClass.T2,
-                                                                    ec2.InstanceSize.MICRO)
-                                            ],
-                                            min_size=2,
-                                            max_size=2,
-                                            desired_size=2
-                                            )
-        admin_user = iam.User.from_user_name(self, 'User', 'adminUser')
-        self.cluster.aws_auth.add_user_mapping(
-            admin_user, groups=["system:masters"])
+        key_pair = ec2.CfnKeyPair(
+            self, f"k8s-{env_name}-keypair", key_name=f"k8s-{env_name}"
+        )
+
+        launch_template = ec2.CfnLaunchTemplate(
+            self,
+            f"k8s-{env_name}-lt",
+            launch_template_data=ec2.CfnLaunchTemplate.LaunchTemplateDataProperty(
+                instance_type="t2.micro",
+                key_name=key_pair.key_name,
+                tag_specifications=[
+                    ec2.CfnLaunchTemplate.TagSpecificationProperty(
+                        resource_type="instance",
+                        tags=[tag(key="Name", value=f"k8s-{env_name}")],
+                    )
+                ],
+            ),
+            launch_template_name=f"k8s-{env_name}",
+        )
+
+        self.cluster.add_nodegroup_capacity(
+            "spot",
+            min_size=2,
+            max_size=2,
+            desired_size=2,
+            nodegroup_name=f"spot-{env_name}",
+            capacity_type=eks.CapacityType.SPOT,
+            launch_template_spec=eks.LaunchTemplateSpec(
+                id=launch_template.ref,
+                version=launch_template.attr_latest_version_number,
+            ),
+            subnets=app_subnets,
+        )
+        admin_user = iam.User.from_user_name(self, "User", "adminUser")
+        self.cluster.aws_auth.add_user_mapping(admin_user, groups=["system:masters"])
--- infrastructure/constructs/networking/vpc.py
+++ infrastructure/constructs/networking/vpc.py
@@ -1,7 +1,4 @@
-from aws_cdk import (
-    aws_ec2 as ec2,
-    aws_ssm as ssm
-)
+from aws_cdk import aws_ec2 as ec2, aws_ssm as ssm
 from constructs import Construct
 
 
@@ -12,25 +9,21 @@ class VPC(Construct):
         # The code that defines your stack goes here
         env_name = self.node.try_get_context("env")
 
-        self.vpc = ec2.Vpc(self, env_name,
-                           ip_addresses=ec2.IpAddresses.cidr(
-                               '192.168.50.0/24'),
-                           max_azs=3,
-                           enable_dns_hostnames=True,
-                           enable_dns_support=True,
-                           subnet_configuration=[
-                               ec2.SubnetConfiguration(
-                                   name='dmz',
-                                   subnet_type=ec2.SubnetType.PUBLIC
-                               ),
-                               ec2.SubnetConfiguration(
-                                   name='app',
-                                   subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
-                               ),
-                               ec2.SubnetConfiguration(
-                                   name='db',
-                                   subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
-                               )
-                           ],
-                           vpc_name=env_name
-                           )
+        self.vpc = ec2.Vpc(
+            self,
+            env_name,
+            ip_addresses=ec2.IpAddresses.cidr("192.168.50.0/24"),
+            max_azs=3,
+            enable_dns_hostnames=True,
+            enable_dns_support=True,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(name="dmz", subnet_type=ec2.SubnetType.PUBLIC),
+                ec2.SubnetConfiguration(
+                    name="app", subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+                ),
+                ec2.SubnetConfiguration(
+                    name="db", subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
+                ),
+            ],
+            vpc_name=env_name,
+        )
